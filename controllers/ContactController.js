498026daff130098091dcc1130c09fde9cb79cd5
trlam07 <trlam1981@gmail.com>
2024-07-01 20:55:18 +0700

    update mail

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -11,23 +11,23 @@ class ContactController {
   //gửi mail liên hệ chủ shop
   static sendEmail = async (req, res) => {
     try {
-    //   const nodemailer = require("nodemailer");
-    //     //cấu hình
-    //   const transporter = nodemailer.createTransport({
-    //     host: "smtp.gmail.com",
-    //     port: 465,
-    //     secure: true, // Use `true` for port 465, `false` for all other ports
-    //     auth: {
-    //       user: "trlam1981@gmail.com",
-    //       pass: "adbcbcbcb", //không phải mật khẩu
-    //     },
-    //   });
-    //   await transporter.sendMail({
-    //     from: "trlam1981@gmail.com", // sender address
-    //     to: "trlam1981@gmail.com", // list of receivers
-    //     subject: "Hello", // Subject line
-    //     html: "<b>Hello godashop</b>", // html body
-    //   });
+      const nodemailer = require("nodemailer");
+        //cấu hình
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // Use `true` for port 465, `false` for all other ports
+        auth: {
+          user: "sonlamhbl2022@gmail.com",
+          pass: "cvjg uqww lafj zcbs", //không phải mật khẩu
+        },
+      });
+      await transporter.sendMail({
+        from: "sonlamhbl2022@gmail.com", // sender address
+        to: "trlam1981@gmail.com", // list of receivers
+        subject: "Hello", // Subject line
+        html: "<b>Hello godashop</b>", // html body
+      });
 
       res.end("Gửi mail thành công!!!");
     } catch (error) {
