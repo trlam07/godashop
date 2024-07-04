dc7f25b3885dd8502c2703946a0ae46bc5a86832
trlam07 <trlam1981@gmail.com>
2024-07-04 19:59:20 +0700

    feat: update đăng ký

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
-const customerModel = require('../models/Customer')
+const customerModel = require('../models/Customer');
+const bcrypt = require('bcrypt');
 class AuthController {
     static login = async(req, res) => {
         try {
@@ -15,7 +16,34 @@ class AuthController {
                 });
                 return;
             }
-            res.end('Login thành công!!!')          
+
+            if(!bcrypt.compareSync(password, customer.password)) {
+                console.log('password', password)
+                console.log('customer.password', customer.password)
+                req.session.message_error = 'Lỗi: mật khẩu không đúng!';
+                //sau khi session được lưu thì sẽ điều hướng đến trang chủ
+                req.session.save(() => {
+                    res.redirect('/')
+                });
+                return;
+            }
+
+            if(!customer.is_active) {
+                req.session.message_error = `Lỗi: tài khoản chưa kích hoạt, vui lòng nhập  email!`;
+                //sau khi session được lưu thì sẽ điều hướng đến trang chủ
+                req.session.save(() => {
+                    res.redirect('/')
+                });
+                return;
+            }
+            req.session.name = customer.name;
+            req.session.email = customer.email;
+            req.session.message_success = 'Đã đăng nhập thành công!'
+            req.session.save(() => {
+                //điều hướng vào trang thông tin cá nhân (làm sau)
+                res.redirect('/')
+            });
+
         } catch (error) {
             res.status(500).send(error.message)
         }
